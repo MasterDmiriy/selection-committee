23e983fa15c60dda0daaa981f49d4f49d3c5b400
Дима Струнов <negativnuy@gmail.com>
2019-10-13 14:28:34 +0300

    Добавлены "статические" таблицы

---
+++ SelectionCommittee.DAL/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SelectionCommittee.DAL.Entities
+{
+    public class City
+    {
+        [Key]
+        public  int Id { set; get; }
+        public string Name { get; set; }
+    }
+}
