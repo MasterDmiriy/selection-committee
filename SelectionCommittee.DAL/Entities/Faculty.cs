90c83f1a2b236057189590036243a4c7368c4afd
Дима Струнов <negativnuy@gmail.com>
2019-10-13 16:01:59 +0300

    Добавлены факультет и специальность

---
+++ SelectionCommittee.DAL/Entities/Faculty.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SelectionCommittee.DAL.Entities
+{
+    public class Faculty
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public SubjectEIE FirstSubjectEIE { get; set; }
+
+        [ForeignKey("FirstSubjectEIE")]
+        public int FirstSubject { get; set; }
+
+        public SubjectEIE SecondSubjectEIE { get; set; }
+
+        [ForeignKey("SecondSubjectEIE")]
+        public int SecondSubject { get; set; }
+
+        public SubjectEIE ThirdSubjectEIE { get; set; }
+
+        [ForeignKey("ThirdSubjectEIE")]
+        public int ThirdSubject { get; set; }
+
+        public SubjectEIE FourthSubjectEIE { get; set; }
+
+        [ForeignKey("FourthSubjectEIE")]
+        public int? FourthSubject { get; set; }
+
+        public IList<Specialty> Specialties { get; set; }
+
+        public Faculty()
+        {
+            Specialties = new List<Specialty>();
+        }
+    }
+}
---
+++ SelectionCommittee.DAL/Entities/Specialty.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SelectionCommittee.DAL.Entities
+{
+     public class Specialty
+    {
+        [Key]
+        public int Number { get; set; }
+
+        public string Name { get; set; }
+
+        public int BudgetPlaces { get; set; }
+
+        public int TotalPlaces { get; set; }
+
+        public Region Region { get; set; }
+
+        [ForeignKey("Region")]
+        public int RegionId { get; set; }
+        public Faculty Faculty { get; set; }
+
+        [ForeignKey("Faculty")]
+        public int FacultyId { get; set; }
+
+    }
+}
