7d2ee975a66c97f71ee438f767b31c2e281f52d4
Дима Струнов <negativnuy@gmail.com>
2019-10-13 17:12:55 +0300

    Добавлены классы по управлению пользователями и ролями

---
+++ SelectionCommittee.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNet.Identity;
+using SelectionCommittee.DAL.Entities;
+
+namespace SelectionCommittee.DAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, string> store) : base(store)
+        {
+        }
+    }
+}
---
+++ SelectionCommittee.DAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity;
+using SelectionCommittee.DAL.Entities;
+
+namespace SelectionCommittee.DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+    }
+}
